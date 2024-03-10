34791e545b2cc4bd590dc687aece7a620a3a4f9d
rjassef <roberto.assef@mail.udp.cl>
2024-03-09 17:13:32 -0800

    Updated file lists and logs.

--- distribute_files.py
+++ distribute_files.py
@@ -4,13 +4,21 @@ import numpy as np
 from astropy.io import fits
 import subprocess
 import os
+from astropy.table import Table
 
 #Make a list of all the fits files.
 ls_output = subprocess.run("ls rawdata/*.fits", shell=True, capture_output=True)
 fnames = ls_output.stdout.decode('utf8').split()
 
+#Load the blacklist of files. 
+black_list = Table.read("blacklist.txt", format='ascii.no_header')
+
 #Now, go by file and put them in the directory they belong.
 for fname in fnames:
+
+    #Check if the file is blacklisted. If so, skip. 
+    if fname[8:] in black_list['col1']:
+        continue
 
     #Open the fits file.
     h = fits.open(fname)
