51d6b3626e3d504a85a426d436ecdf525c5d8626
rjassef <roberto.assef@mail.udp.cl>
2024-03-11 12:55:52 -0700

    Added radius parameter

--- analysis/run_pol.py
+++ analysis/run_pol.py
@@ -2,7 +2,7 @@ from get_pol import get_pol
 import filter_fitsfiles
 from myparse import myparse
 
-def run_pol(obj_name, obs_params, ex_ref, ey_ref):
+def run_pol(obj_name, obs_params, ex_ref, ey_ref, r_ap=1.0, save_output=True):
 
     args = myparse()
 
@@ -15,22 +15,23 @@ def run_pol(obj_name, obs_params, ex_ref, ey_ref):
         ob_ids = obs_params[filter]["ob_ids"]
         mjds = obs_params[filter]["mjds"]
 
-        pol_frac, pol_angle, epol_frac, epol_angle = get_pol(obj_name, ex_ref, ey_ref, rim_folder, filter, mask_folder, crz_folder, ob_ids=ob_ids, mjds=mjds, r_ap=1.0, force=args.force_new)
+        pol_frac, pol_angle, epol_frac, epol_angle = get_pol(obj_name, ex_ref, ey_ref, rim_folder, filter, mask_folder, crz_folder, ob_ids=ob_ids, mjds=mjds, r_ap=r_ap, force=args.force_new)
 
-        fname = "{}_pol_{}".format(obj_name, filter)
-        if args.use_skyflats:
-            fname += "_with_skyflat"
-        fname += ".dat"
-        cato = open(fname,"w")
-        for k, ob_id in enumerate(ob_ids):
-            if obs_params[filter]["name"][k] is not None:
-                cato.write("{0:15s} ".format(obs_params[filter]["name"][k]))
-            else:
-                if ob_id is None:
-                    cato.write("{0:15s} ".format("All"))
+        if save_output:
+            fname = "{}_pol_{}".format(obj_name, filter)
+            if args.use_skyflats:
+                fname += "_with_skyflat"
+            fname += ".dat"
+            cato = open(fname,"w")
+            for k, ob_id in enumerate(ob_ids):
+                if obs_params[filter]["name"][k] is not None:
+                    cato.write("{0:15s} ".format(obs_params[filter]["name"][k]))
                 else:
-                    cato.write("{0:15d} ".format(ob_id))
-            cato.write("{0:10.4f} {1:10.4f} {2:10.2f} {3:10.2f}\n".format(pol_frac[0][k], epol_frac[0][k], pol_angle[0][k], epol_angle[0][k]))
-        cato.close()
+                    if ob_id is None:
+                        cato.write("{0:15s} ".format("All"))
+                    else:
+                        cato.write("{0:15d} ".format(ob_id))
+                cato.write("{0:10.4f} {1:10.4f} {2:10.2f} {3:10.2f}\n".format(pol_frac[0][k], epol_frac[0][k], pol_angle[0][k], epol_angle[0][k]))
+            cato.close()
 
     return
