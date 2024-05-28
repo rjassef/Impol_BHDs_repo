f045de6bc5f3e2ad9908687b3dfbc6984525a41b
rjassef <roberto.assef@mail.udp.cl>
2024-05-28 14:42:38 -0700

    Updated cosmic ray rejection

--- analysis/crz.py
+++ analysis/crz.py
@@ -15,7 +15,7 @@ def crz_clean(fname, mask, rim_folder, crz_folder, force=False):
     except FileNotFoundError:
         print("Cleaning cosmic rays in ",fname)
         h = fits.open("{0:s}/{1:s}".format(rim_folder, fname))
-        crmask, clean_im = detect_cosmics(h[0].data, inmask=mask)
+        crmask, clean_im = detect_cosmics(h[0].data, inmask=mask, objlim=10)
         h[0].data = clean_im
         h.writeto("{0:s}/{1:s}".format(crz_folder, crzname), overwrite=True)
         h.close()
