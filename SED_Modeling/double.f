afa793db0d605feb3d22724e8a66cb8244165f17
rjassef <roberto.assef@mail.udp.cl>
2024-08-06 14:29:17 -0700

    Experimented more with the SED modeling.

--- SED_Modeling/double.f
+++ SED_Modeling/double.f
@@ -44,6 +44,8 @@
 
       real*8 L6um_high, L6um_low
 
+      real*8 mask_lam(3)
+
       z0 = 2.0d0
       nspec = 5
       open(unit=32, file='nchan.txt', status='old')
@@ -132,6 +134,20 @@ c     Set an error floor of 5%
 c     Do not use W1 or W2 if IRAC is available.
 c         if(jyuse(5).eq.1) jyuse(1) = 0
 c         if(jyuse(6).eq.1) jyuse(2) = 0
+
+c     Do not use bands that contain uncommonly bright lines (as seen in W2246 or W1904 for example). 
+c         mask_lam(1) = 0.5007
+c         mask_lam(2) = 0.6563
+c         mask_lam(3) = 0.3727 ![OII]
+   !       nmask = 1
+   !       do nnmask=1,nmask
+   !          do j=1,nchan
+   !             if((lbar(j)-0.3.lt.mask_lam(nnmask)*(1+z)).and.
+   !   *            (lbar(j)+0.3.gt.mask_lam(nnmask)*(1+z))) then
+   !                jyuse(j) = 0
+   !             endif
+   !          enddo
+   !       enddo
 
 c     Get the number of usable bands.
          m = 0
